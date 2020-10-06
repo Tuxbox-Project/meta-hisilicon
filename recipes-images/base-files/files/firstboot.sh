02b8fa42973ed3079f6aae5bf77dd0a496000568
Markus Volk <f_l_k@t-online.de>
2020-10-07 01:05:26 +0200

    firstboot.sh: align script

--- recipes-images/base-files/files/firstboot.sh
+++ recipes-images/base-files/files/firstboot.sh
@@ -9,27 +9,19 @@ if [ -b "/dev/disk/by-partlabel/linuxrootfs" ]; then
 fi
 
 if [ -b /dev/disk/by-partlabel/userdata ]; then
-	if blkid /dev/disk/by-partlabel/userdata | grep "ext4"; then
-		echo "Resizing userdata partition"
-		resize2fs /dev/disk/by-partlabel/userdata
-		if [ ! -f /mnt/userdata/swapfile ]; then
-			[ ! -d /mnt/userdata ] && mkdir /mnt/userdata
-			mount -l /dev/disk/by-partlabel/userdata /mnt/userdata
-			echo "Creating swapfile"
-			dd if=/dev/zero of=/mnt/userdata/swapfile bs=1024 count=204800
-			chmod 600 /mnt/userdata/swapfile
-			mkswap -L swapspace /mnt/userdata/swapfile
-		fi
-	else
-		echo "Setup userdata partition"
-		mkfs.ext4 -F /dev/disk/by-partlabel/userdata
-		echo "Creating swapfile"
-		[ ! -d /mnt/userdata ] && mkdir /mnt/userdata
-		mount -l /dev/disk/by-partlabel/userdata /mnt/userdata
-		dd if=/dev/zero of=/mnt/userdata/swapfile bs=1024 count=204800
-		chmod 600 /mnt/userdata/swapfile
-		mkswap -L swapspace /mnt/userdata/swapfile
-	fi
+	echo "Resizing userdata partition"
+	resize2fs /dev/disk/by-partlabel/userdata
+fi
+
+if [ ! -f /mnt/userdata/swapfile ]; then
+	[ ! -d /mnt/userdata ] && mkdir /mnt/userdata
+	mountpoint -q /mnt/userdata || mount -l /dev/disk/by-partlabel/userdata /mnt/userdata
+	echo "Creating swapfile"
+	dd if=/dev/zero of=/mnt/userdata/swapfile bs=1024 count=204800
+	chmod 600 /mnt/userdata/swapfile
+	mkswap -L swapspace /mnt/userdata/swapfile
+	swapon /mnt/userdata/swapfile
+	grep -q /mnt/userdata/swapfile /etc/fstab || echo "/mnt/userdata/swapfile none swap defaults 0 0" >> /etc/fstab
 fi
 
 echo "first boot script work done"
@@ -39,3 +31,4 @@ echo "firstboot script disabled"
 echo > /dev/dbox/oled0
 
 [ -f /usr/bin/etckeeper ] && /etc/etckeeper/update_etc.sh
+
